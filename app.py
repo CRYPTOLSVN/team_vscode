51ca864c7ee264031867bab0bd216c6ca204b61e
miyahara <y.miyahara.sys20@morijyobi.ac.jp>
2021-03-30 12:38:59 +0900

    練習問題
    練習問題⒉

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,url_for,request,flash
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def top():
     return render_template(("index.html"))
+
+@app.route("/hankei")
+def hankei():
+    return render_template("hankei.html")
+
+@app.route("/kekka" methods=['POST'])
+def kekka():
+    hankei=repuest.form.get("hankei")
+    ensyu=(haneki+hankei) * 3.14
+    menseki=(hankei*hankei) * 3.14
+
+    return render_template("kekka.html",ensyu=ensyu,menseki=menseki)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
